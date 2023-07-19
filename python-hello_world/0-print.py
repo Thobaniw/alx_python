25ecaef63a823fc9fec7a5a43ae3d0daaf2e9d00
Thobaniw <Gamathobani9@gmail.com>
2023-07-19 22:10:55 +0200

    puzzle

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,9 +1,2 @@
-expected_output = '''Programming is like building a multilingual puzzle
-[stderr]: 
-(0 chars long)'''
 
-output= "Programming is like building a multilingual puzzle"
-if output == expected_output:
-    print("Programming is like building a multilingual puzzle")
-else:
-    print("Programming is like building a multilingual puzzle")
+print('"Programming is like building a multilingual puzzle')
