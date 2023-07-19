79d29cef5630a2b2d3d5342180c332ccf7c79c96
Thobaniw <Gamathobani9@gmail.com>
2023-07-19 21:51:55 +0200

    loops 5

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,5 @@
 
 for tens_digit in range(9):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:02d}".format(tens_digit * 10 + ones_digit), end=", ")
-print()
+        print("{:02d}".format(tens_digit * 10 + ones_digit), end=",")
+print("89")
