034cf6398d3066b03d1c0734c1072df25aa90738
Thobaniw <Gamathobani9@gmail.com>
2023-07-20 20:12:32 +0200

    power

---
+++ python-functions/1-power.py
@@ -0,0 +1,29 @@
+def pow(a, b):
+    # Handle negative exponent correctly
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    result = 1
+    while b:
+        if b & 1:
+            result *= a
+        a *= a
+        b >>= 1
+
+    return result
+result = pow(2, 2)
+print(result) 
+
+result = pow(98, 2)
+print(result) 
+
+result = pow(98, 0)
+print(result) 
+
+result = pow(100, -2)
+print(result)  
+
+result = pow(-4, 5)
+print(result)  
+
