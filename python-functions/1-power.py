9162fe916361db2aa76d204251f90b3a46e1f678
Thobaniw <Gamathobani9@gmail.com>
2023-07-23 17:39:54 +0200

    task 1

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,6 +1,11 @@
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+    else:
+        result = 1.0
+        for _ in range(abs(b)):
+            result /= a
     return result
 
