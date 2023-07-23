b6f458baf9fdecc9e5412c01ca5105159ca22342
Thobaniw <Gamathobani9@gmail.com>
2023-07-23 17:22:28 +0200

    Task 1

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,10 +1,2 @@
-def add(a, b):
-    # Handle negative numbers correctly
-    if b >= 0:
-        while b:
-            a, b = a ^ b, (a & b) << 1
-    else:
-        while b:
-            a, b = a ^ b, ((~a) & b) << 1
-    return a
-
+def add(a , b):
+    return a + b
