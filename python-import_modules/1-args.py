ee55628e8d0596db357fb12167d1335b8361c5aa
Thobaniw <Gamathobani9@gmail.com>
2023-07-25 19:29:50 +0200

    Task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,6 +1,7 @@
 import sys
-if __name__== "_main_":
-    count = len(sys.argv)-1
+
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments")
     elif count == 1:
