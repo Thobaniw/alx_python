0a040e7d5551e1069a09ac9229499dbfa727776f
Thobaniw <Gamathobani9@gmail.com>
2023-07-26 17:19:47 +0200

    practice

--- python-import_modules/main_0.py
+++ python-import_modules/main_0.py
@@ -1,16 +1,9 @@
-def import_raise_exception():
-  """Imports the raise_exception module."""
-
-  raise_exception = __import__('4-raise_exception')
-  return raise_exception.raise_exception
-
-
-def main():
-  """Raises an exception."""
-
-  raise_exception = import_raise_exception()
-  raise_exception()
-
-
-if __name__ == "__main__":
-  main()
+multiTable = [[0]*10 for i in range(10)]
+for i in range(1,10):
+    for j in range(1,10):
+        multiTable[i][j] = i * j
+for i in range(1,10):
+     for j in range(1,10):
+            print(multiTable[i][j], end=",")
+print()
+#multiplication table
