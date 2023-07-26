418f966607209b9ef08ef6e3e71d5be52a271252
Thobaniw <Gamathobani9@gmail.com>
2023-07-26 13:57:21 +0200

    Task 4

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,10 +1,11 @@
 def raise_exception():
-    raise TypeError("This is a type exception.")
+    raise TypeError("Exception has been raised")
 
 try:
     raise_exception()
 except TypeError as te:
-    print("Exception raised")
+    print("Exception has been raised")
+
 
 
 
