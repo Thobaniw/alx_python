57c92220490e9d8a91ac1a47c026c793ff2d3cd8
Thobaniw <Gamathobani9@gmail.com>
2023-07-26 14:22:55 +0200

    task 5

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -2,8 +2,8 @@ def raise_exception_msg(msg):
     raise NameError(msg)
 
 try:
-    # Test case 2: message = "Python is cool"
-    raise_exception_msg("Python is cool")
+    # Test case 1: message = "C is fun"
+    raise_exception_msg("C is fun")
 except NameError as ne:
     print(ne)
 
