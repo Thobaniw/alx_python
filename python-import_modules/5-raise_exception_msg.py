35b26a2d1c6ff97218d958a0eac308bcf3ca1046
Thobaniw <Gamathobani9@gmail.com>
2023-07-26 14:25:59 +0200

    task 5

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,10 +1,4 @@
-def raise_exception_msg(msg):
-    raise NameError(msg)
-
-try:
-    # Test case 1: message = "C is fun"
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+def raise_exception_msg(message=""):
+    raise NameError(message)
 
 
