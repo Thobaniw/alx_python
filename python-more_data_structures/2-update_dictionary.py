12edb329d5a80bd6140f2b2d96459ca0f606a754
Thobaniw <Gamathobani9@gmail.com>
2023-07-28 17:15:43 +0200

    Task 2

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,16 +1,25 @@
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    for k, v in a_dictionary.items():
-        print(f"{k}: {v}")
-    print("xx")
-    return a_dictionary
+    """Replaces or adds key/value in a dictionary.
+
+    Args:
+        a_dictionary (dict): The dictionary to update.
+        key (str): The key to update.
+        value: The value to set for the key.
+
+    Returns:
+        None.
+    """
+
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
 
 
-# Test cases
-a_dictionary = {'a': "a", 'b': "b", 'c': "c", 'd': "d", 'e': "e"}
-update_dictionary(a_dictionary.copy(), "a", "A")
-update_dictionary(a_dictionary.copy(), "e", "E")
-# update_dictionary(a_dictionary.copy(), "a", 89)
-# update_dictionary(a_dictionary.copy(), "e", [1, 2, 3])
-# update_dictionary(a_dictionary.copy(), "f", "A")
-# pdate_dictionary({}, "a", "a")
+a_dictionary = {'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'}
+
+update_dictionary(a_dictionary, 'a', 'A')
+update_dictionary(a_dictionary, 'f', 'F')
+
+for key, value in a_dictionary.items():
+    print(f'{key}: {value}')
