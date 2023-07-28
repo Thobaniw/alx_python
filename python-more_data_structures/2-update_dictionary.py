c05a13625c902005bac0e4a24bdc0c8fb33e039f
Thobaniw <Gamathobani9@gmail.com>
2023-07-28 18:25:21 +0200

    Task 2

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,17 +1,3 @@
-def print_sorted_dictionary(my_dict):
-    keys = sorted(my_dict.keys())
-    for key in keys:
-        print(f"{key}: {my_dict[key]}")
-
-
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-
-
-# Test case
-a_dictionary = {'a': "a", 'b': "b", 'c': "c", 'd': "d", 'e': "e"}
-new_dict = update_dictionary(a_dictionary.copy(), "a", "A")
-print_sorted_dictionary(new_dict)
-print("xx")
-print_sorted_dictionary(a_dictionary.copy())
