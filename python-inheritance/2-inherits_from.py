9b1921b9d856720847e60af8ecf6c8940ba5febb
Thobaniw <Gamathobani9@gmail.com>
2023-08-03 21:27:19 +0200

    Task 2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -15,4 +15,5 @@ def inherits_from(obj, a_class):
     Returns:
     bool: True if obj is an instance of a subclass of a_class; False otherwise.
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    return (issubclass(type(obj), a_class) and
+            type(obj) is not a_class)
