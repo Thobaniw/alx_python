9d2d36fadc73097ea87d47582996adc8667593be
Thobaniw <Gamathobani9@gmail.com>
2023-08-03 19:15:31 +0200

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,15 @@
 class Square:
     def __init__(self, size):
         self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def perimeter(self):
+        return 4 * self.__size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, size):
+        self.__size = size
