2b0b86ac502d0dff6e9385ab0753486d2ce645af
Thobaniw <Gamathobani9@gmail.com>
2023-08-03 19:57:07 +0200

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,36 @@
 class Square:
+    """
+    This class defines a square.
+
+    Private instance attribute:
+    __size (int): The size of the square.
+
+    Instantiation with size (no type/value verification).
+    """
 
     def __init__(self, size):
+        """
+        Initializes a new square with the given size.
+
+        Parameters:
+        size (int): The size of the square.
+        """
         self.__size = size
 
-    @property
-    def size(self):
-        return self.__size
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+        int: The area of the square.
+        """
+        return self.__size * self.__size
+
+    def perimeter(self):
+        """
+        Calculates the perimeter of the square.
+
+        Returns:
+        int: The perimeter of the square.
+        """
+        return 4 * self.__size
