381fb43497e443559072d80a86bd967e6ec27c01
Thobaniw <Gamathobani9@gmail.com>
2023-08-10 18:10:11 +0200

    Task 1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,67 @@
+# In models/rectangle.py
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    The Rectangle class inherits from the Base class and represents a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The x-coordinate of the rectangle's position.
+        y (int): The y-coordinate of the rectangle's position.
+
+    Methods:
+        __init__(self, width, height, x=0, y=0, id=None): Class constructor to initialize a Rectangle instance.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's position. Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle's position. Defaults to 0.
+            id (int, optional): The ID of the instance. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
