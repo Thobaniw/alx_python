066871d5eb77bf0e65bc50f0d5d711f520bacf57
Thobaniw <Gamathobani9@gmail.com>
2023-08-10 18:25:40 +0200

    Task 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,36 +1,31 @@
-# In models/rectangle.py
+"""
+This module contains the Rectangle class that inherits from Base.
+"""
 
 from models.base import Base
-
-'''
-This module contains a rectangle class that inherits from base class
-'''
 
 
 class Rectangle(Base):
     """
-    The Rectangle class inherits from the Base class and represents a rectangle.
+    Represents a rectangle, inheriting from the Base class.
 
     Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-        x (int): The x-coordinate of the rectangle's position.
-        y (int): The y-coordinate of the rectangle's position.
-
-    Methods:
-        __init__(self, width, height, x=0, y=0, id=None): Class constructor to initialize a Rectangle instance.
+        width (int): Width of the rectangle.
+        height (int): Height of the rectangle.
+        x (int): X-coordinate position of the rectangle.
+        y (int): Y-coordinate position of the rectangle.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Initialize a Rectangle instance.
+        Initializes a Rectangle instance.
 
         Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the rectangle's position. Defaults to 0.
-            y (int, optional): The y-coordinate of the rectangle's position. Defaults to 0.
-            id (int, optional): The ID of the instance. Defaults to None.
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int, optional): X-coordinate position of the rectangle. Defaults to 0.
+            y (int, optional): Y-coordinate position of the rectangle. Defaults to 0.
+            id (int, optional): Unique identifier of the rectangle. Defaults to None.
         """
         super().__init__(id)
         self.width = width
@@ -40,32 +35,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """int: The width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width of the rectangle."""
         self.__width = value
 
     @property
     def height(self):
+        """int: The height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height of the rectangle."""
         self.__height = value
 
     @property
     def x(self):
+        """int: The X-coordinate position of the rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Sets the X-coordinate position of the rectangle."""
         self.__x = value
 
     @property
     def y(self):
+        """int: The Y-coordinate position of the rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Sets the Y-coordinate position of the rectangle."""
         self.__y = value
