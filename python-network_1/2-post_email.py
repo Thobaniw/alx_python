5bfb5cc5625d5b357e12f0411204f178b3fbe701
Thobaniw <Gamathobani9@gmail.com>
2023-08-11 10:24:39 +0200

    Task 2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,6 +3,7 @@ import requests
 This import requests to be able to access get
 '''
 import sys
+
 '''
 This import sys to be able to pass a commmand line
 '''
@@ -35,9 +36,8 @@ if __name__ == "__main__":
     This script takes a URL and an email address as input, sends a POST request to the URL with the email as a parameter,
     and displays the body of the response. It uses the 'requests' and 'sys' packages.
     """
-    # Prompt the user to enter the URL and email
     url = input("Enter the URL: ")
-    email = input("Enter the email address: ")
+    email = input("Email: ")
 
     # Call the function to send the POST request and display the response
     send_post_request(url, email)
