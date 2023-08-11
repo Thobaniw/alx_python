8497b241e62ac222306de0cc90de6a89e83b683f
Thobaniw <Gamathobani9@gmail.com>
2023-08-11 04:30:17 +0200

    Task 0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+import requests
+
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
