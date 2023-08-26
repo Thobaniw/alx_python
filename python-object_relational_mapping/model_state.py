35efc84d8ef9fb9368f2bae6a18b11f9f759cb50
Thobaniw <Gamathobani9@gmail.com>
2023-08-26 08:47:12 +0200

    Task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Create a connection to the MySQL server
+DATABASE_URL = 'mysql://username:password@localhost:3306/database_name'
+engine = create_engine(DATABASE_URL)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create a base class for declarative models
+Base = declarative_base()
+
+# Define the State class
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+# Create the table in the database
+Base.metadata.create_all(engine)
+
+# Close the session
+session.close()
