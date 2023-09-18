9708d60fe26eb588a25c0778d5e54ac13f2abca2
Thobaniw <Gamathobani9@gmail.com>
2023-09-18 22:55:54 +0200

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 import requests
 
-# Make a GET request to a URL
+# Make a GET request to the URL
 response = requests.get("https://intranet.hbtn.io/status")
 
 # Print the status code of the response
 print("Status code:", response.status_code)
+
+# Print the body response content
+print("\nBody response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
